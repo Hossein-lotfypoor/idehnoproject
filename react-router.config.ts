d1a84c4054406e807e92df65ff9ec82d9464c50e
Hossein Lotfypoor <148619164+Hossein-lotfypoor@users.noreply.github.com>
2026-06-25 01:31:01 +0330

    fixed for githubpage

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+ basename: process.env.NODE_ENV === 'production' ? '/idehnoproject/' : '/',
   ssr: false,
 } satisfies Config;
